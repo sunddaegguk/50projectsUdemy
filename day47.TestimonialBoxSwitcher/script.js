e6acb113fd1a8fbafae3af7ae417000093e57b83
sunddaegguk <ssitek22@gmail.com>
2024-01-10 23:58:41 +0900

    day47 ~day49

--- day47.TestimonialBoxSwitcher/script.js
+++ day47.TestimonialBoxSwitcher/script.js
@@ -1,94 +1,70 @@
-const quizData = [
+const testimonialsContainer = document.querySelector('.testimonials-container');
+const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.user-image');
+const username = document.querySelector('.username');
+const role = document.querySelector('.role');
+
+const testimonials = [
   {
-    question: 'Which language runs in a web browser?',
-    a: 'Java',
-    b: 'C',
-    c: 'Python',
-    d: 'JavaScript',
-    correct: 'd',
+    name: 'Miyah Myles',
+    position: 'Marketing',
+    photo:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
+    text: "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
   },
   {
-    question: 'What does CSS stand for?',
-    a: 'Central Style Sheets',
-    b: 'Cascading Style Sheets',
-    c: 'Cascading Simple Sheets',
-    d: 'Cars SUVs Sailboats',
-    correct: 'b',
+    name: 'June Cha',
+    position: 'Software Engineer',
+    photo: 'https://randomuser.me/api/portraits/women/44.jpg',
+    text: 'This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
   },
   {
-    question: 'What does HTML stand for?',
-    a: 'Hypertext Markup Language',
-    b: 'Hypertext Markdown Language',
-    c: 'Hyperloop Machine Language',
-    d: 'Helicopters Terminals Motorboats Lamborginis',
-    correct: 'a',
+    name: 'Iida Niskanen',
+    position: 'Data Entry',
+    photo: 'https://randomuser.me/api/portraits/women/68.jpg',
+    text: "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
   },
   {
-    question: 'What year was JavaScript launched?',
-    a: '1996',
-    b: '1995',
-    c: '1994',
-    d: 'none of the above',
-    correct: 'b',
+    name: 'Renee Sims',
+    position: 'Receptionist',
+    photo: 'https://randomuser.me/api/portraits/women/65.jpg',
+    text: "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
+  },
+  {
+    name: 'Jonathan Nunfiez',
+    position: 'Graphic Designer',
+    photo: 'https://randomuser.me/api/portraits/men/43.jpg',
+    text: "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
+  },
+  {
+    name: 'Sasha Ho',
+    position: 'Accountant',
+    photo:
+      'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
+    text: 'This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
+  },
+  {
+    name: 'Veeti Seppanen',
+    position: 'Director',
+    photo: 'https://randomuser.me/api/portraits/men/97.jpg',
+    text: 'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
   },
 ];
 
-const quiz = document.getElementById('quiz');
-const answerEls = document.querySelectorAll('.answer');
-const questionEl = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
+let idx = 1;
+function updateTestimonial() {
+  const { name, position, photo, text } = testimonials[idx];
 
-let currentQuiz = 0;
-let score = 0;
-loadQuiz();
+  testimonial.innerHTML = text;
+  userImage.src = photo;
+  username.innerHTML = name;
+  role.innerHTML = position;
 
-function loadQuiz() {
-  deselectAnswers();
+  idx++;
 
-  const currentQuizData = quizData[currentQuiz];
-
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerHTML = currentQuizData.b;
-  c_text.innerHTML = currentQuizData.c;
-  d_text.innerHTML = currentQuizData.d;
-}
-
-function deselectAnswers() {
-  answerEls.forEach((answerEl) => (answerEl.checked = false));
-}
-
-function getSelected() {
-  let answer;
-  answerEls.forEach((answerEl) => {
-    if (answerEl.checked) {
-      answer = answerEl.id;
-    }
-  });
-  return answer;
-}
-
-submitBtn.addEventListener('click', () => {
-  const answer = getSelected();
-  if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
-      score++;
-    }
-
-    currentQuiz++;
-
-    if (currentQuiz < quizData.length) {
-      loadQuiz();
-    } else {
-      quiz.innerHTML = `
-          <h2>You answered ${score} / ${quizData.length} questions correctly</h2>
-
-          <button onclick="location.reload()">Reload</button>
-          `;
-    }
+  if (idx > testimonials.length - 1) {
+    idx = 0;
   }
-});
+}
+
+setInterval(updateTestimonial, 10000);
